bc50aad42a72b5ad611be3274923d0f09d252485
gowthamkasthuri143-cpu <gowthamkasthuri143@gmail.com>
2026-06-01 23:32:41 +0530

    Delete route (8).js

--- route (8).js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearSessionCookie } from "@/lib/auth";
-
-export async function POST() {
-  const response = NextResponse.json({ message: "Logged out" });
-  clearSessionCookie(response);
-  return response;
-}
